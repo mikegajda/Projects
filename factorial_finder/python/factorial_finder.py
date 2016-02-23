b7a06a9c7ec7e15c4717e3d5a4e0fa14b8cc0334
Ernest Dever <e@ernestdever.com>
2016-02-22 22:58:33 -0500

    Added iterative factorial finder

--- factorial_finder/python/factorial_finder.py
+++ factorial_finder/python/factorial_finder.py
@@ -1,7 +1,15 @@
-def factorial_finder(number):
+def factorial_finder_recursive(number):
 	if number == 1:
 		return number
 	else:
-		return number * factorial_finder(number - 1)
+		return number * factorial_finder_recursive(number - 1)
 
-print(factorial_finder(10))
+def factorial_finder_iterative(number):
+	total = 1
+	while number > 1:
+		total *= number
+		number -= 1
+	return total
+
+print(factorial_finder_recursive(10))
+print(factorial_finder_iterative(10))
